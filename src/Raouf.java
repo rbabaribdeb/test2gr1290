7bb5077fc5c264e1bb8634006593917ff1938ef9
Thibaut Méchain <116610147+TibMec@users.noreply.github.com>
2025-09-17 10:06:11 -0400

    - classe Etudiant et listeEtudiants ajoutés

--- src/Raouf.java
+++ src/Raouf.java
@@ -1,8 +1,16 @@
+import java.util.ArrayList;
 
 public class Raouf {
+    private ArrayList<Etudiant> listeEtudiants;
+
+    public Raouf() {
+        listeEtudiants = new ArrayList<>();
+    }
+
     public void enseigner(){
 
     }
+
     public void gererAutorisations(){
 
     }
